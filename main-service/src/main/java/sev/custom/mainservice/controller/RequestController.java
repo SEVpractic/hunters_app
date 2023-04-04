700e0ef74c00d98a2c5fba40f6a5c2b4acadf6c2
SEVpractic <wisebrain@yandex.ru>
2023-04-04 22:44:57 +0300

    feat: Создать контроллер запросов

---
+++ main-service/src/main/java/sev/custom/mainservice/controller/RequestController.java
@@ -0,0 +1,40 @@
+package sev.custom.mainservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping(path = "/requests")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Validated
+public class RequestController {
+    private final RequestServise requestServise;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public requestDto create(@Validated(CreateValidationGroup.class) requestIncomeDto dto) {
+        return requestServise.create(dto);
+    }
+
+    @PatchMapping(path = "/{requestId}")
+    public requestDto update(@Validated(UpdateValidationGroup.class) requestIncomeDto dto,
+                             @PathVariable("requestId") @Positive long requestId) {
+        return requestServise.update(requestId);
+    }
+
+    @GetMapping(path = "/{requestId}")
+    public requestDto get(@PathVariable("requestId") @Positive long requestId) {
+        return requestServise.get(requestId);
+    }
+
+    @DeleteMapping(path = "/{requestId}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("requestId") @Positive long requestId) {
+        requestServise.delete(requestId);
+    }
+}
